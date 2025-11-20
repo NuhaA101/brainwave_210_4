620c030b8b1c0403e30dac66df0d346b80713eec
NuhaA101 <nuhaahmad09@gmail.com>
2025-11-19 19:46:20 -0600

    Added fragments to show other ways of adding content

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,12 @@
 import Section from "../components/ui/Section.jsx";
 import Container from "../components/ui/Container.jsx";
+import { fragments } from "../data/FragmentsData.jsx"
 
 export default function About() {
   return (
     <Section>
       <Container>
-        <h1 className="heading-page">About Me</h1>
-        <p className="mt-2 body-default">Placeholder content.</p>
+        {fragments.content}
       </Container>
     </Section>
   );
---
+++ src/Pages/Fragments.jsx
@@ -0,0 +1,14 @@
+import { fragments } from "../data/FragmentsData.jsx"
+import Section from "../components/ui/Section.jsx";
+import Container from "../components/ui/Container.jsx";
+
+
+export default function Fragments() {
+    return (
+         <Section>
+              <Container>
+                {fragments.content}
+              </Container>
+            </Section>
+    );
+}
